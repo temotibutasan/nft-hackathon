59c004144afd410b37cdc86303fb86f00ccd0045
temotibutasan <Jukeb0x13@yahoo.co.jp>
2022-05-05 02:47:49 +0900

    コントラクトの修正

--- Promote.sol
+++ Promote.sol
@@ -10,31 +10,33 @@ contract NFT is ERC721Enumerable, Ownable {
   struct UserStruct {
     uint index;
   }
-  
   mapping(address => UserStruct) private userStructs;
   address[] private userIndex;
   address[] private luckyAddress;
-  event LogNewUser(address indexed userAddress, uint index);
 
 
   using Strings for uint256;
+  // https://gateway.pinata.cloud/ipfs/QmQtfYemFqU7sWW4mPK9LdBeDozDHLsN2VmikqpZRYwGXq?filename=Lucky.svg
   string public luckyUri;
+  // https://gateway.pinata.cloud/ipfs/QmS8KvUTmRzQQr5QPPKaTZWTWxrvXQfewqMN4jXz3X3KFr/UnLucky.svg
   string public unLuckyUri;
+  string public drewUri;
   uint256 public maxSupply = 1;
   uint256 public maxLucky = 10;
   uint256 public probability = 3000;
-  bool public drawingResult = false;
+  uint256 public drawingResult = 2;
 
   constructor(
-    string memory _initLuckyURI,
-    string memory _initUnLuckyUri
-  ) ERC721("OnChainPromoteNft", "OCPN") {
-    setLuckyURI(_initLuckyURI);
-    setUnUnLuckyURI(_initUnLuckyUri);
+    string memory _initLuckyUri,
+    string memory _initUnLuckyUri,
+    string memory _initDrewUri
+  ) ERC721("PromoteNft", "PMNFT") {
+    setLuckyURI(_initLuckyUri);
+    setUnLuckyURI(_initUnLuckyUri);
+    setDrewURI(_initDrewUri);
     mint(1);
   }
 
-  // public
   function mint(uint256 _mintAmount) public payable {
     uint256 supply = totalSupply();
     require(_mintAmount > 0);
@@ -57,19 +59,26 @@ contract NFT is ERC721Enumerable, Ownable {
       "ERC721Metadata: URI query for nonexistent token"
     );
     
-    if(drawingResult){
+    if(drawingResult == 0){
+      return drewUri;
+    }
+    else if(drawingResult == 1){
         return luckyUri;
     } else{
         return unLuckyUri;
     }
   }
   
-  function setLuckyURI(string memory _newURI) public onlyOwner {
+  function setLuckyURI(string memory _newURI) private onlyOwner {
     luckyUri = _newURI;
   }
 
-  function setUnUnLuckyURI(string memory _newURI) public onlyOwner {
+  function setUnLuckyURI(string memory _newURI) private onlyOwner {
     unLuckyUri = _newURI;
+  }
+
+  function setDrewURI(string memory _newURI) private onlyOwner {
+    drewUri = _newURI;
   }
 
 
@@ -82,10 +91,10 @@ contract NFT is ERC721Enumerable, Ownable {
     require(_isApprovedOrOwner(_msgSender(), tokenId), "ERC721: transfer caller is not owner nor approved");
 
     if(isUser(to)) {
-        drawingResult = false;
+        drawingResult = 0;
     } else {
-        drawingResult = drawing(tokenId);
-        if(drawingResult == true){
+        drawingResult = drawing(tokenId)? 1:2;
+        if(drawingResult == 1){
             luckyAddress.push(to);
         }
         userIndex.push(to);
@@ -105,7 +114,7 @@ contract NFT is ERC721Enumerable, Ownable {
     return uint256(keccak256(abi.encodePacked(input)));
   }
 
-  function drawing(uint256 tokenId) public
+  function drawing(uint256 tokenId) private
     view
     returns (bool) 
   {
@@ -118,7 +127,7 @@ contract NFT is ERC721Enumerable, Ownable {
     if(rand % probability == 0){
         return true;
     } else {
-        return true;
+        return false;
     }
   }
 
@@ -129,38 +138,34 @@ contract NFT is ERC721Enumerable, Ownable {
     return (userIndex[userStructs[userAddress].index] == userAddress);
   }
 
-    function insertUser(address userAddress) 
-    public
-    returns(bool)
-    {
-        if(isUser(userAddress)) return false;
-        userIndex.push(userAddress);
-        userStructs[userAddress].index = userIndex.length-1;
-        LogNewUser(
-            userAddress, 
-            userStructs[userAddress].index);
-        return true;
-    }
+  function insertUser(address userAddress) private
+  returns(bool)
+  {
+      if(isUser(userAddress)) return false;
+      userIndex.push(userAddress);
+      userStructs[userAddress].index = userIndex.length-1;
+      return true;
+  }
 
-     function toString(uint256 value) internal pure returns (string memory) {
-    // Inspired by OraclizeAPI's implementation - MIT license
-    // https://github.com/oraclize/ethereum-api/blob/b42146b063c7d6ee1358846c198246239e9360e8/oraclizeAPI_0.4.25.sol
+    function toString(uint256 value) internal pure returns (string memory) {
+  // Inspired by OraclizeAPI's implementation - MIT license
+  // https://github.com/oraclize/ethereum-api/blob/b42146b063c7d6ee1358846c198246239e9360e8/oraclizeAPI_0.4.25.sol
 
-        if (value == 0) {
-            return "0";
-        }
-        uint256 temp = value;
-        uint256 digits;
-        while (temp != 0) {
-            digits++;
-            temp /= 10;
-        }
-        bytes memory buffer = new bytes(digits);
-        while (value != 0) {
-            digits -= 1;
-            buffer[digits] = bytes1(uint8(48 + uint256(value % 10)));
-            value /= 10;
-        }
-        return string(buffer);
-    }
+      if (value == 0) {
+          return "0";
+      }
+      uint256 temp = value;
+      uint256 digits;
+      while (temp != 0) {
+          digits++;
+          temp /= 10;
+      }
+      bytes memory buffer = new bytes(digits);
+      while (value != 0) {
+          digits -= 1;
+          buffer[digits] = bytes1(uint8(48 + uint256(value % 10)));
+          value /= 10;
+      }
+      return string(buffer);
+  }
 }
