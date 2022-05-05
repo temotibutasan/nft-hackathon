c1dcb0f39f645069dbae406d98974aa4977fb1f0
temotibutasan <Jukeb0x13@yahoo.co.jp>
2022-05-06 02:18:32 +0900

    画像表示修正 (#2)
    * 修正
    
    * コントラクトの修正
    
    * 画像表示テストの追加
    
    * ファイルの置き場所変更
    
    * 画像表示修正

--- Promote.sol
+++ Promote.sol
@@ -6,35 +6,28 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract NFT is ERC721Enumerable, Ownable {
-
   struct UserStruct {
     uint index;
   }
-  
   mapping(address => UserStruct) private userStructs;
   address[] private userIndex;
   address[] private luckyAddress;
-  event LogNewUser(address indexed userAddress, uint index);
-
 
   using Strings for uint256;
-  string public luckyUri;
-  string public unLuckyUri;
   uint256 public maxSupply = 1;
   uint256 public maxLucky = 10;
   uint256 public probability = 3000;
-  bool public drawingResult = false;
+  uint256 public drawingResult = 2;
 
-  constructor(
-    string memory _initLuckyURI,
-    string memory _initUnLuckyUri
-  ) ERC721("OnChainPromoteNft", "OCPN") {
-    setLuckyURI(_initLuckyURI);
-    setUnUnLuckyURI(_initUnLuckyUri);
+  constructor() ERC721("PromoteNft", "PMNFT") {
     mint(1);
   }
 
-  // public
+  // internal
+  function _baseURI() internal pure override returns (string memory) {
+      return "https://gateway.pinata.cloud/ipfs/QmaeoJwShoQJYrPdaiifZUwLbKNoPFzPcdcKNT2nDNBJMR/";
+  }
+
   function mint(uint256 _mintAmount) public payable {
     uint256 supply = totalSupply();
     require(_mintAmount > 0);
@@ -45,33 +38,12 @@ contract NFT is ERC721Enumerable, Ownable {
     }
   }
 
-  function tokenURI(uint256 tokenId)
-    public
-    view
-    virtual
-    override
-    returns (string memory)
-  {
-    require(
-      _exists(tokenId),
-      "ERC721Metadata: URI query for nonexistent token"
-    );
-    
-    if(drawingResult){
-        return luckyUri;
-    } else{
-        return unLuckyUri;
-    }
+  function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
+      require(_exists(tokenId), "ERC721Metadata: URI query for nonexistent token");
+      string memory baseURI = _baseURI();
+      string memory resultURI = drawingResult == 0 ? "lucky.json":"unlucky.json"; 
+      return bytes(baseURI).length > 0 ? string(abi.encodePacked(baseURI,resultURI)) : "";
   }
-  
-  function setLuckyURI(string memory _newURI) public onlyOwner {
-    luckyUri = _newURI;
-  }
-
-  function setUnUnLuckyURI(string memory _newURI) public onlyOwner {
-    unLuckyUri = _newURI;
-  }
-
 
   function transferFrom(
     address from,
@@ -80,36 +52,30 @@ contract NFT is ERC721Enumerable, Ownable {
   ) public virtual override {
     //solhint-disable-next-line max-line-length
     require(_isApprovedOrOwner(_msgSender(), tokenId), "ERC721: transfer caller is not owner nor approved");
-
-    if(isUser(to)) {
-        drawingResult = false;
-    } else {
-        drawingResult = drawing(tokenId);
-        if(drawingResult == true){
-            luckyAddress.push(to);
-        }
-        userIndex.push(to);
-    }
+    updateResult(to,tokenId);
     _transfer(from, to, tokenId);
   }
 
-  function safeTransferFrom(
-        address from,
-        address to,
-        uint256 tokenId
-  ) public virtual override {
-    safeTransferFrom(from, to, tokenId, "");
+  function updateResult(address to,uint256 tokenId) public {
+    if(isUser(to)) {
+      drawingResult = 0;
+    } else {
+      drawingResult = drawing(tokenId)? 1:2;
+      if(drawingResult == 1){
+          luckyAddress.push(to);
+      }
+      userIndex.push(to);
+    }
   }
 
   function random(string memory input) internal pure returns (uint256) {
     return uint256(keccak256(abi.encodePacked(input)));
   }
 
-  function drawing(uint256 tokenId) public
+  function drawing(uint256 tokenId) private
     view
     returns (bool) 
   {
-
     if(luckyAddress.length >= maxLucky){
         return false;
     }
@@ -118,7 +84,7 @@ contract NFT is ERC721Enumerable, Ownable {
     if(rand % probability == 0){
         return true;
     } else {
-        return true;
+        return false;
     }
   }
 
@@ -129,38 +95,34 @@ contract NFT is ERC721Enumerable, Ownable {
     return (userIndex[userStructs[userAddress].index] == userAddress);
   }
 
-    function insertUser(address userAddress) 
-    public
-    returns(bool)
-    {
-        if(isUser(userAddress)) return false;
-        userIndex.push(userAddress);
-        userStructs[userAddress].index = userIndex.length-1;
-        LogNewUser(
-            userAddress, 
-            userStructs[userAddress].index);
-        return true;
-    }
+  function insertUser(address userAddress) private
+  returns(bool)
+  {
+      if(isUser(userAddress)) return false;
+      userIndex.push(userAddress);
+      userStructs[userAddress].index = userIndex.length-1;
+      return true;
+  }
 
-     function toString(uint256 value) internal pure returns (string memory) {
-    // Inspired by OraclizeAPI's implementation - MIT license
-    // https://github.com/oraclize/ethereum-api/blob/b42146b063c7d6ee1358846c198246239e9360e8/oraclizeAPI_0.4.25.sol
+  function toString(uint256 value) internal pure returns (string memory) {
+  // Inspired by OraclizeAPI's implementation - MIT license
+  // https://github.com/oraclize/ethereum-api/blob/b42146b063c7d6ee1358846c198246239e9360e8/oraclizeAPI_0.4.25.sol
 
-        if (value == 0) {
-            return "0";
-        }
-        uint256 temp = value;
-        uint256 digits;
-        while (temp != 0) {
-            digits++;
-            temp /= 10;
-        }
-        bytes memory buffer = new bytes(digits);
-        while (value != 0) {
-            digits -= 1;
-            buffer[digits] = bytes1(uint8(48 + uint256(value % 10)));
-            value /= 10;
-        }
-        return string(buffer);
-    }
+      if (value == 0) {
+          return "0";
+      }
+      uint256 temp = value;
+      uint256 digits;
+      while (temp != 0) {
+          digits++;
+          temp /= 10;
+      }
+      bytes memory buffer = new bytes(digits);
+      while (value != 0) {
+          digits -= 1;
+          buffer[digits] = bytes1(uint8(48 + uint256(value % 10)));
+          value /= 10;
+      }
+      return string(buffer);
+  }
 }
